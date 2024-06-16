e9ff1dc291d150707e671c4fa2ee1297b8cfab5b
rakeshpurata <rakeshpurata123@gmail.com>
2024-06-16 17:30:05 +0530

    Add files via upload

---
+++ greatestnum.py
@@ -0,0 +1,27 @@
+'''a, b, c = map(int, input("Enter three numbers: ").split())
+if (a >= b & a >= c):
+    print(a," is the greatest number.")
+elif (b >= c):
+    print(b," is the largest number.")
+else:
+    print(c,"is the largest number.")'''
+
+
+'''a, b, c = map(int, input("Enter three numbers: ").split())
+if (a >= b and a >= c):
+    print(a," is the largest number.")
+elif ( b >= c):
+    print(b," is the largest number.")
+else:
+    print(c,"is the largest number.")'''
+
+
+'''Name="Hitam"
+age=20
+print(f"{Name} is {age} years old")
+print("{0} is {1} years old".format(Name,age))
+print("Name is {}".format(Name))'''
+
+'''a=5//3
+b=2**2
+print(b)'''
