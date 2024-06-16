0167a8b3bd9d79dab3bf72c856b9e89676642cbb
rakeshpurata <rakeshpurata123@gmail.com>
2024-06-16 17:37:59 +0530

    Add files via upload

---
+++ Recursion.py
@@ -0,0 +1,51 @@
+#direct recursion:
+
+# def fact(n):
+#     if n==0:
+#         return 1
+#     else:
+#         return n*fact(n-1)
+# print(fact(5))
+
+#indirect recursion:
+
+# def a(n):
+#     if n>0:
+#         return b(n-1) 
+#     else:
+#         return True
+# def b(n):
+#     if n>0:
+#         return a(n-1)
+#     else:
+#         return False
+# print(a(5))
+
+# Python program to display the Fibonacci sequence
+
+# def recur_fibo(n):
+#    if n <= 1:
+#        return n
+#    else:
+#        return(recur_fibo(n-1) + recur_fibo(n-2))
+
+# nterms = 10
+# if nterms <= 0:
+#    print("Plese enter a positive integer")
+# else:
+#    print("Fibonacci sequence:")
+#    for i in range(nterms):
+#        print(recur_fibo(i))
+
+
+
+
+
+
+
+
+
+
+
+
+
