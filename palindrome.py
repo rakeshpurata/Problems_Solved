320e7d7b5600998d4a76a5e04dcae13aec37d13e
rakeshpurata <rakeshpurata123@gmail.com>
2024-06-16 17:36:41 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,31 +1,3 @@
-# string=input()
-# def palindrom(string):
-#     half=len(string)//2
-#     print(half)
-#     if len(string)%2==0:
-#         first_string= string[:half]
-#         second_string=string[half:]
-#     else:
-#         first_string=string[:half]
-#         second_string=string[half+1:]
-#     if first_string == second_string[::-1]:
-#       print('palindrome')
-#     else:
-#         print('not a palindrome')
-# print(palindrom(string))
-
-
-
-# def palindrom(s):
-#     if s==s[::-1]:
-#         return True
-#     else:
-#         return False
-# s=input()
-# if palindrom(s):
-#     print("YES")
-# else:
-#     print("No")
 
 n= int(input())
 temp=n
